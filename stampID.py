f23f532994ceac6037f6454bd162c62676c223d3
JacobLycan <77426285+JacobLycan@users.noreply.github.com>
2021-01-13 22:34:07 -0800

    Add files via upload

--- stampID.py
+++ stampID.py
@@ -1,15 +1,21 @@
+#A program to identify Canadian stamps by image
 import cv2
-import os 
+import os
 
-
+#Folder paths
 templatePath =  'StampTemplate'
 imagePath = "StampImages"
 textPath = "StampInfo"
+myList = os.listdir(templatePath) 
+
 orb = cv2.ORB_create(nfeatures=1000)
-images =[]
-classNames = []
-myList = os.listdir(templatePath)
+images =[] #list of different stamps
+classNames = [] #names of different stamps
+
+
+
 def findDes(images):
+    #Find the descriptors of a list of images
     desList =[]
     for img in images:
         kp,des= orb.detectAndCompute(img,None)
@@ -20,15 +26,16 @@ def findDes(images):
 
 
 
-def findID(img,desList):
-    kp2,des2 = orb.detectAndCompute(img,None)
+def findStamp(stamp,desList):
+    #find the closest stamp to the provided image
+    kp2,des2 = orb.detectAndCompute(stamp,None)
     bf = cv2.BFMatcher()
     matchList =[]
     finalVal =-1
     for des in desList:
         matches = bf.knnMatch(des,des2, k=2)
 
-# Apply ratio test
+        # Apply ratio test
         good = []
         for m,n in matches:
             if m.distance < 0.75*n.distance:
@@ -39,24 +46,28 @@ def findID(img,desList):
     if len(matchList)!=0:
         finalVal = matchList.index(max(matchList))
     return finalVal
-print(myList)
+
+
+
+#For every stamp in the template folder find a descriptor
 for cl in myList:
     imgCur = cv2.imread(f'{templatePath}/{cl}',0)
     images.append(imgCur)
     desList = findDes(images)
     classNames.append(os.path.splitext(cl)[0])
-print(classNames)
 
-img = "hi"
+
+img = ""
 while img!="Quit":
+    #Get a user provided image and find a matching stamp 
     try:
         img = input("Enter a jpg file that is in the current directory ")
         img2 = cv2.imread(f'{imagePath}/{img}',0)
         origImg = cv2.imread(f'{imagePath}/{img}',-1)
-        id = findID(img2,desList)
+        id = findStamp(img2,desList)
         if id!=-1:
             try:
-                f = open(textPath+"/"+classNames[id]+".txt", "r")
+                f = open(textPath+"/"+classNames[id]+".txt", "r") #Find the info about the stamp
                 print(f.read())
             except:
                 pass
